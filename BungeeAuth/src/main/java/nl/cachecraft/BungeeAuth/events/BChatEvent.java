240eaee6d1932e8fbdf74a7ab3e9486eb3045952
ntaheij <noah@taheij.nl>
2019-06-02 13:44:43 +0200

    Added that you can also use permissions

--- BungeeAuth/src/main/java/nl/cachecraft/BungeeAuth/events/BChatEvent.java
+++ BungeeAuth/src/main/java/nl/cachecraft/BungeeAuth/events/BChatEvent.java
@@ -7,6 +7,7 @@ import net.md_5.bungee.api.event.ChatEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import nl.cachecraft.BungeeAuth.Main;
+import nl.cachecraft.BungeeAuth.events.Check.CheckMain;
 
 public class BChatEvent implements Listener {
 	
@@ -25,7 +26,7 @@ public class BChatEvent implements Listener {
 			  {
 				  Main.tries.put(p.getUniqueId(), 1);
 			  }
-			  if (Check.normal())
+			  if (CheckMain.normal())
 			  {
 				  Log.info(prefix + "§7Login Try [" + p.getName() + "] " + Main.tries.get(p.getUniqueId()) + "/" + Main.max_tries + " Main.tries.");
 			  }
@@ -33,16 +34,16 @@ public class BChatEvent implements Listener {
 			  if (tries_player > (Main.max_tries - 1)) {
 				  String kick_message = Main.cg.getString("kick-message").replaceAll("&", "§");
 				  p.disconnect(prefix + "\n" + kick_message);
-				  if (Check.normal() || Check.dev())
+				  if (CheckMain.normal() || CheckMain.dev())
 				  {
-					  Log.info(prefix + "[" + p.getName() + "]" + "Kicking player..");
+					  Log.info(prefix + "[" + p.getName() + "]" + " Kicking player..");
 				  }
 				  Main.tries.remove(p.getUniqueId());
 				  if (Main.cg.getBoolean("temp-ban"))
 				  {
-					  if (Check.dev())
+					  if (CheckMain.dev())
 					  {
-						  Log.info(prefix + "[" + p.getName() + "]" + "Tempbanning player..");
+						  Log.info(prefix + "[" + p.getName() + "]" + " Tempbanning player..");
 					  }
 					  String tempban_cmd = "";
 					  tempban_cmd = Main.cg.getString("temp-ban-command").replaceAll("<player>", p.getName());
@@ -55,12 +56,12 @@ public class BChatEvent implements Listener {
 				  try
 				  {  
 					  int code = Integer.valueOf(msg);
-					  if(Check.playerInputCode(p, code))
+					  if(CheckMain.playerInputCode(p, code))
 					  {
 						  Main.authlocked.remove(p.getUniqueId());
-						  if (Check.dev() || Check.normal())
+						  if (CheckMain.dev() || CheckMain.normal())
 						  {
-							  Log.info(prefix + "[" + p.getName() + "]" + "Code is valid.");
+							  Log.info(prefix + "[" + p.getName() + "]" + " Code is valid.");
 						  }
 						  p.sendMessage(prefix + "§aAuthentication succesful. Welcome §9" + p.getName() + "§a.");
 						  Main.tries.remove(p.getUniqueId());
@@ -71,9 +72,9 @@ public class BChatEvent implements Listener {
 						  int tries_t = Main.tries.get(p.getUniqueId());
 						  Main.tries.remove(p.getUniqueId());
 						  Main.tries.put(p.getUniqueId(), tries_t + 1);
-						  if (Check.dev() || Check.normal())
+						  if (CheckMain.dev() || CheckMain.normal())
 						  {
-							  Log.info(prefix + "[" + p.getName() + "]" + "Code is invalid.");
+							  Log.info(prefix + "[" + p.getName() + "]" + " Code is invalid.");
 						  }
 						  p.sendMessage(prefix + "§cIncorrect or expired code. ** A code will only contain numbers **");  
 						  e.setCancelled(true);
@@ -81,9 +82,9 @@ public class BChatEvent implements Listener {
 				  }
 				  catch (Exception ex)
 				  {
-					  if (Check.dev() || Check.normal())
+					  if (CheckMain.dev() || CheckMain.normal())
 					  {
-						  Log.info(prefix + "[" + p.getName() + "]" + "Code is invalid.");
+						  Log.info(prefix + "[" + p.getName() + "]" + " Code is invalid.");
 					  }
 					  int tries_t = Main.tries.get(p.getUniqueId());
 					  Main.tries.remove(p.getUniqueId());
@@ -102,11 +103,11 @@ public class BChatEvent implements Listener {
 			    	if (p.hasPermission("bauth.reload") || p.getName().equalsIgnoreCase("NTaheij"))
 			    	{
 			    		Main.registerConfig();
-			    		p.sendMessage(prefix + " " + "§aReloaded.");
+			    		p.sendMessage(prefix + "§aReloaded.");
 			    	}
 			    	else 
 			    	{
-			    		p.sendMessage(prefix + " " + "§9You don't have permission to use this command.");
+			    		p.sendMessage(prefix + "§9You don't have permission to use this command.");
 			    	}
 			    	e.setCancelled(true);
 			    }
